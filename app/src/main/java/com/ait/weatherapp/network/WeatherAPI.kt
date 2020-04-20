0a3bbe6891c7b6d87bd2ae6d0b32af2f36c9d083
Sagar Punjabi <sagar.punjabi@yahoo.com>
2020-04-20 11:44:33 -0400

    Added weather API

---
+++ app/src/main/java/com/ait/weatherapp/network/WeatherAPI.kt
@@ -0,0 +1,15 @@
+package com.ait.weatherapp.network
+
+import com.ait.weatherapp.data.WeatherResult
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherAPI {
+    @GET("data/2.5/weather")
+    fun getWeatherDetails(
+        @Query("q") city: String,
+        @Query("units") units: String,
+        @Query("appid") appid: String
+    ): Call<WeatherResult>
+}
