52366edd6ca9993a854957c5c07023640ecce4ca
Sagar Punjabi <sagar.punjabi@yahoo.com>
2020-04-22 18:10:00 -0400

    handled cities dont exist

--- app/src/main/java/com/ait/weatherapp/WeatherDetailsActivity.kt
+++ app/src/main/java/com/ait/weatherapp/WeatherDetailsActivity.kt
@@ -8,6 +8,7 @@ import com.afollestad.materialdialogs.input.getInputField
 import com.ait.weatherapp.data.WeatherResult
 import com.ait.weatherapp.network.WeatherAPI
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_weather_details.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,36 +20,63 @@ class WeatherDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_details)
+        var city = intent.getStringExtra("CITY_NAME")
+        tvCity.text = city
 
-//        var retrofit = Retrofit.Builder()
-//            .baseUrl("https://api.openweathermap.org")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        var weatherAPI = retrofit.create(WeatherAPI::class.java)
-//        var inputField: MaterialDialog
+        var retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        var weatherAPI = retrofit.create(WeatherAPI::class.java)
 
 
-//        var weatherCall = weatherAPI.getWeatherDetails(
-//            dialogBox.getInputField().text.toString(),
-//            "imperial",
-//            "9b4905abe2faa7c4b0235e30ec4af256"
-//        )
-//        weatherCall.enqueue(object : Callback<WeatherResult> {
-//            override fun onResponse(call: Call<WeatherResult>, response: Response<WeatherResult>) {
-//                Snackbar.make(view, "${response.message()}", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//
-//                Log.d("Success","${response.message()}")
-//                Log.d("Success","${response.body()}")
-//            }
-//            override fun onFailure(call: Call<WeatherResult>, t: Throwable) {
-//                Snackbar.make(view, "${t.message}", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//
-//                Log.d("Error","${t.message}")
-//            }
-//        })
+        var weatherCall = weatherAPI.getWeatherDetails(
+            city,
+            "imperial",
+            "9b4905abe2faa7c4b0235e30ec4af256"
+        )
+        weatherCall.enqueue(object : Callback<WeatherResult> {
+            override fun onResponse(call: Call<WeatherResult>, response: Response<WeatherResult>) {
+                if (response.message().equals("Not Found")) {
+                    MaterialDialog(this@WeatherDetailsActivity).show {
+                        title(R.string.error)
+                        message(R.string.city_not_found)
+                        cancelable(false)
+                        cancelOnTouchOutside(false)
+                        positiveButton(R.string.go_back) { dialog ->
+                            finish()
+                        }
+                    }
+                } else {
+                    var weatherData = response.body()
+
+                    tvTemp.text = weatherData?.main?.temp.toString()
+                    tvSunsetTime.text = weatherData?.sys?.sunset.toString()
+                    tvSunRiseTime.text = {
+                        val sdf = java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                        val date = java.util.Date(weatherData?.sys?.sunrise.toString())
+                        sdf.format(date)
+                    }.toString()
+                }
+
+                Log.d("Success", "${response.message()}")
+                Log.d("Success", "${response.body()}")
+            }
+
+            override fun onFailure(call: Call<WeatherResult>, t: Throwable) {
+                MaterialDialog(this@WeatherDetailsActivity).show {
+                    title(R.string.error)
+                    message(text = t.message)
+                    cancelable(false)
+                    cancelOnTouchOutside(false)
+                    positiveButton(R.string.go_back) { dialog ->
+                        finish()
+                    }
+                }
+
+            }
+        })
 
     }
 }
